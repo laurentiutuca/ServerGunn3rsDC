6785ec750a607071e226a48e526601755e1a4605
Laur <laurentiu.tuca@gmail.com>
2013-07-09 22:29:29 +0300

    modified server 10:29 at 7/9/13

--- src/com/gunn3rs/main/Main.java
+++ src/com/gunn3rs/main/Main.java
@@ -1,11 +1,16 @@
 package com.gunn3rs.main;
 
+/**
+ * 
+ * @author Laur
+ * run with no parameters for CLI(only this for now anyway
+ * 
+ */
 public class Main {
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
---
+++ src/com/gunn3rs/servercli/ServerCLI.java
@@ -0,0 +1,27 @@
+package com.gunn3rs.servercli;
+
+import java.util.Scanner;
+
+import com.gunn3rs.servercliinterface.IServerCLI;
+
+public class ServerCLI implements IServerCLI,Runnable{
+
+	@Override
+	public String readCommand(Scanner sc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean solveCommand(String comm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
